cddf49fc0a122e867d653d7d031507a4439914da
Ryan <shucksterbomb@gmail.com>
2024-08-27 19:33:30 -0400

    Added Various Modes
    Added different modes to Rock, Paper, Scissors where you can either play with 1 or 2 players. If you choose 1 then you play against a computer and if you choose 2 then you play with another person on the same computer. I also added a play again feature where the player can say yes or no if they want to play again

--- games/rock_paper_scissor.cpp
+++ games/rock_paper_scissor.cpp
@@ -2,23 +2,51 @@
 #include <cstdlib>
 #include <time.h>
 
-int main() {
-    /*
-    * Purpose: This program's purpose is to replicate Rock, Paper, Scissors game where a user chooses either rock, paper, or scissor and then the computer makes a decision and then the program determines who is the winner.
-    * Author: Ryan Shuck
-    * Language: C++ (CLion 2024.1.1 g++ compiler)
-    * Date of creation: 4/26/24
-    * Date of last revision: 4/27/24
-    * Citation of sources: None
-    */
+   /*
+   * Purpose: This program's purpose is to replicate Rock, Paper, Scissors game where you can play with either 1 or 2 players. If you choose 1 player then you will be played against a computer and if you choose 2 players then you will be played against another player. You can play either mode as many times as you want as long as you say "yes" or "no" when it asks you if you want to play again.
+   * Author: Ryan Shuck
+   * Language: C++ (CLion 2024.1.1 g++ compiler)
+   * Date of creation: 4/26/24
+   * Date of last revision: 4/27/24
+   * Citation of sources: None
+   */
 
-    srand(time(0));
+//Functions
+void play_game();
+void vs_com();
+void vs_player();
+void play_again();
 
-    std::string input = "";
+void play_game(){
+    int num_players;
 
     std::cout << "Welcome to Rock, Paper, Scissors! A classic game where you and your friend forms one of the three shapes in the game!" << std::endl;
-    std::cout << "In this version, you are playing against a computer which is player 2" << std::endl;
-    std::cout << "To start the game, Player 1 must enter either rock, paper, or scissor first then the computer will make a decision" << std::endl;
+    std::cout << "Enter how many players are playing (1 or 2): " << std::endl;
+    std::cin >> num_players;
+
+    if(num_players != 1 && num_players != 2){
+        std::cout << "I'm sorry but you didn't enter 1 or 2. Try again!" << std::endl;
+        std::cout << "Enter how many players are playing (1 or 2): " << std::endl;
+        std::cin >> num_players;
+    }
+
+    if(num_players == 1){
+        vs_com();
+    }
+    else{
+        vs_player();
+    }
+}
+
+void vs_com(){
+    //Vs Computer Mode
+    srand(time(0));
+
+    std::string input;
+
+    std::cout << "You typed in one player." << std::endl;
+    std::cout << "In this version, you are playing against a computer which is player 2." << std::endl;
+    std::cout << "To start the game, Player 1 must enter either rock, paper, or scissor first then the computer will make a decision." << std::endl;
     std::cout << "Enter your move (rock, paper, scissor): " << std::endl;
     std::cin >> input;
 
@@ -32,7 +60,7 @@ int main() {
 
     //Computer's move
     int rand_num = rand() % 3;
-    std::string com_move = "";
+    std::string com_move;
 
     switch(rand_num){
         case 0:
@@ -69,8 +97,91 @@ int main() {
     else if(input == "paper" && com_move == "scissor"){
         std::cout << "Scissor beats Paper! Computer wins!" << std::endl;
     }
-    //Situations if both players pick the same move
+    //Situations if both the player and computer pick the same move
     else if((input == "rock" && com_move == "rock") || (input == "paper" && com_move == "paper") || (input == "scissor" && com_move == "scissor")){
         std::cout << "You both picked the same move! Try again!" << std::endl;
     }
+
+    play_again();
 }
+
+void vs_player(){
+    //Vs Player Mode
+    std::string move_1;
+    std::string move_2;
+
+    std::cout << "You entered two players." << std::endl;
+    std::cout << "In this version, you are playing against another player which could be your friend or anyone." << std::endl;
+    std::cout << "To start the game, Player 1 must enter either rock, paper, or scissor first then Player 2 will must enter rock, paper, or scissor." << std::endl;
+    std::cout << "Enter your move Player 1 (rock, paper, scissor): " << std::endl;
+    std::cin >> move_1;
+
+    if(move_1 != "rock" && move_1 != "paper" && move_1 != "scissor"){
+        std::cout << "I'm sorry but you didn't enter rock, paper, or scissor. Try again!" << std::endl;
+        std::cout << "Enter your move Player 1: " << std::endl;
+        std::cin >> move_1;
+    }
+
+    std::cout << "Enter your move Player 2 (rock, paper, scissor): " << std::endl;
+    std::cin >> move_2;
+
+    if(move_2 != "rock" && move_2 != "paper" && move_2 != "scissor"){
+        std::cout << "I'm sorry but you didn't enter rock, paper, or scissor. Try again!" << std::endl;
+        std::cout << "Enter your move Player 2: " << std::endl;
+        std::cin >> move_2;
+    }
+
+    std::cout << "Player 1 has chosen " << move_1 << "!" << std::endl;
+    std::cout << "Player 2 has chosen " << move_2 << "!" << std::endl;
+
+    //Player 1 Win Situations
+    if(move_1 == "rock" && move_2 == "scissor"){
+        std::cout << "Rock beats Scissor! Player 1 wins!" << std::endl;
+    }
+    else if(move_1 == "paper" && move_2 == "rock"){
+        std::cout << "Paper beats Rock! Player 1 wins!" << std::endl;
+    }
+    else if(move_1 == "scissor" && move_2 == "paper"){
+        std::cout << "Scissor beats Paper! Player 1 wins!" << std::endl;
+    }
+    //Player 2 Win Situations
+    else if(move_1 == "scissor" && move_2 == "rock"){
+        std::cout << "Rock beats Scissor! Player 2 wins!" << std::endl;
+    }
+    else if(move_1 == "rock" && move_2 == "paper"){
+        std::cout << "Paper beats Rock! Player 2 wins!" << std::endl;
+    }
+    else if(move_1 == "paper" && move_2 == "scissor"){
+        std::cout << "Scissor beats Paper! Player 2 wins!" << std::endl;
+    }
+    //Situations if both players pick the same move
+    else if((move_1 == "rock" && move_2 == "rock") || (move_1 == "paper" && move_2 == "paper") || (move_1 == "scissor" && move_2 == "scissor")){
+        std::cout << "You both picked the same move! Try again!" << std::endl;
+    }
+
+    play_again();
+}
+
+void play_again(){
+    //Play again
+    std::string play_again;
+
+    std::cout << "Did you want to play again? (Enter yes or no)" << std::endl;
+    std::cin >> play_again;
+
+    if(play_again != "yes" && play_again != "no"){
+        std::cout << "I'm sorry but you didn't enter yes or no. Try again!" << std::endl;
+        std::cout << "Enter yes or no: " << std::endl;
+        std::cin >> play_again;
+    }
+
+    if(play_again == "yes"){
+        play_game();
+    }
+}
+
+//Driver function
+int main() {
+    play_game();
+}
+
